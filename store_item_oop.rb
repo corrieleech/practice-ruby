7d9f75a3b99923e82785e83fd95cefe362bac251
Corrie <corrieleech@gmail.com>
2022-01-05 14:00:19 -0600

    adds lines of code to technically complete exercise

--- store_item_oop.rb
+++ store_item_oop.rb
@@ -33,5 +33,11 @@ class Item
 end
 
 item1 = Item.new("pencil", "purple", 3.99, 100)
+item2 = Item.new("pen", "black", 4.99, 250)
 
 puts "Hi this iz Corzstore. Would you like a #{item1.name}? It is #{item1.color} and costs $#{item1.price}."
+
+puts "Oh, you're interested in a #{item2.name}?"
+puts "The price of the item is $#{item2.price}."
+puts "The color of the item is #{item2.color}."
+puts "We have #{item2.stock} in stock."
